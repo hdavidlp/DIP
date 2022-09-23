7ac2e9f1820f702708687457d9195a77164edbc8
hdavidlp <heclop@hotmail.com>
2022-09-23 10:14:29 -0700

    Example Project without Solid Principle Dependency Inversion Principle

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using DemoLibrary;
+
+Person owner = new Person
+{
+    FirstName = "Tim",
+    LastName = "Corey",
+    EmailAddress = "tim@corey.com",
+    PhoneNumber = "6459999999"
+};
+
+Chore chore = new Chore
+{
+    ChoreName = "Take out the trash",
+    Owner = owner
+};
+
+chore.PerformedWork(3);
+chore.PerformedWork(1.5);
+chore.CompleteChore();
+
+
+
---
+++ DemoLibrary/Chore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public  class Chore
+    {
+        public string ChoreName { set; get; }
+        public Person Owner { set; get; }   
+        public double HoursWorked { get; private set; }
+        public bool IsComplete { get; set; }    
+
+        public void PerformedWork(double hours)
+        {
+            HoursWorked += hours;
+            Logger log = new Logger();
+            log.Log($"Performed work on {ChoreName}");
+        }
+
+        public void CompleteChore()
+        {
+            IsComplete = true;
+            
+            Logger log = new Logger();
+            log.Log($"Completed { ChoreName}");
+
+            Emailer emailer = new Emailer();
+            emailer.SendEmail(Owner, $"The chore {ChoreName} is complete.");
+        }
+
+    }
+}
